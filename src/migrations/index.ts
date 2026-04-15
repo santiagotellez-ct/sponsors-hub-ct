d256e2c896c75fa170442aa599bc8d4d7abaf748
santiago Tellez <setr7706@gmail.com>
2026-04-15 02:35:23 -0500

    Reset database and drop migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260415_071808_add_api_keys from './20260415_071808_add_api_keys';
-
-export const migrations = [
-  {
-    up: migration_20260415_071808_add_api_keys.up,
-    down: migration_20260415_071808_add_api_keys.down,
-    name: '20260415_071808_add_api_keys'
-  },
-];
